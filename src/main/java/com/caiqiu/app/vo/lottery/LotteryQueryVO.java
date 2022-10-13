bfeeec2f037a30b851e76270fa522a12ae557def
liubo <3564729256@qq.com>
2022-10-13 17:38:54 +0800

    更新

--- src/main/java/com/caiqiu/app/vo/lottery/LotteryQueryVO.java
+++ src/main/java/com/caiqiu/app/vo/lottery/LotteryQueryVO.java
@@ -6,8 +6,8 @@ import lombok.Data;
 public class LotteryQueryVO {
     private Long pageNo = 1L;//当前页码
     private Long pageSize = 30L;//每页显示数量
-    private Long start_issueNumber;//开始期号
-    private Long end_issueNumber;//结束期号
+    private Long start_issueNumber = 0L;//开始期号
+    private Long end_issueNumber = 0L;//结束期号
     private boolean isAsc = false;//是否升序 默认降序
 
 }
