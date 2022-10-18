a6b644ad345f9c9f365e85dabdce0c59fa473423
liubo <3564729256@qq.com>
2022-10-18 08:53:56 +0800

    登录页面

--- src/main/java/com/caiqiu/app/model/entity/Lottery.java
+++ src/main/java/com/caiqiu/app/model/entity/Lottery.java
@@ -11,7 +11,7 @@ import lombok.EqualsAndHashCode;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author yyyz
@@ -24,7 +24,7 @@ public class Lottery implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-      @TableId(value = "id", type = IdType.AUTO)
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /**
